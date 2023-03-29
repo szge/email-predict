90900ad1ef4edef5b4595a64892cf1671e9b461e
Jonathan <jchiangconan@gmail.com>
2023-03-28 22:07:47 -0400

    almost done

--- create_npy_inputs.py
+++ create_npy_inputs.py
@@ -22,7 +22,7 @@ def get_stats() -> None:
             outf.write(f"{code_name}: {round(sum(codes == get_evt_idx(code)) / len(codes), 3)}\n")
 
 
-NUM_FEATURES = 43
+NUM_FEATURES = 12
 
 # {
 #     user_id: {
@@ -38,6 +38,7 @@ NUM_FEATURES = 43
 #     }
 # }
 user_newsletter_events = {}
+user_events = {}  # maps user id (int) to list of all events (dict)
 
 
 def extract_features() -> npt.NDArray[np.float64]:
@@ -63,8 +64,8 @@ def extract_features() -> npt.NDArray[np.float64]:
         newsletter_id = int(event["newsletter_id"])
         if user_id not in user_newsletter_events:
             user_newsletter_events[user_id] = {}
-            user_newsletter_events[user_id]["opened"] = set()
-            user_newsletter_events[user_id]["unopened"] = set()
+            user_newsletter_events[user_id]["opened"] = []
+            user_newsletter_events[user_id]["unopened"] = []
             user_newsletter_events[user_id]["newsletters"] = {}
         if newsletter_id not in user_newsletter_events[user_id]["newsletters"]:
             user_newsletter_events[user_id]["newsletters"][newsletter_id] = []
@@ -75,16 +76,18 @@ def extract_features() -> npt.NDArray[np.float64]:
             if code in opened_codes:
                 if newsletter_id in user_newsletter_events[user_id]["unopened"]:
                     user_newsletter_events[user_id]["unopened"].remove(newsletter_id)
-                user_newsletter_events[user_id]["opened"].add(newsletter_id)
+                user_newsletter_events[user_id]["opened"].append(newsletter_id)
             else:
-                user_newsletter_events[user_id]["unopened"].add(newsletter_id)
+                user_newsletter_events[user_id]["unopened"].append(newsletter_id)
 
     print("Sorting events by timestamp...")
-    for user_id in user_newsletter_events:
-        for newsletter_id in user_newsletter_events[user_id]["newsletters"]:
+    for user_id, user_info in user_newsletter_events.items():
+        for newsletter_id in user_info["newsletters"]:
             user_newsletter_events[user_id]["newsletters"][newsletter_id].sort(
                 key=lambda ev: datetime.strptime(ev["timestamp"], "%Y-%m-%d %H:%M:%S")
             )
+        user_info["opened"].sort()
+        user_info["unopened"].sort()
 
     print("Creating features...")
 
@@ -95,17 +98,21 @@ def extract_features() -> npt.NDArray[np.float64]:
     feats = np.zeros((count, NUM_FEATURES + 1))
 
     # extract features
+    count = 0
     print("Extracting features...")
     for user_id, user_info in user_newsletter_events.items():
         for newsletter_id in user_info["newsletters"]:
-            preferences = user_prefs[user_id]
+            preferences = user_prefs[str(user_id)]
             feats[count] = np.append(
                 extract_event_features(user_id, user_info, newsletter_id, preferences),
                 user_has_read_newsletter(user_id, newsletter_id)
             )
+            count += 1
 
     np.set_printoptions(threshold=np.inf)
     print(feats[-10:, :])
+    np.set_printoptions(threshold=np.inf)
+    print(feats[:10, :])
 
     return feats
 
@@ -113,29 +120,28 @@ def extract_features() -> npt.NDArray[np.float64]:
 def user_sent_newsletter(user_id: int, newsletter_id: int) -> bool:
     if user_id not in user_newsletter_events:
         return False
-    return newsletter_id in user_newsletter_events[user_id]
+    return newsletter_id in user_newsletter_events[user_id]["newsletters"]
 
 
 def get_events(user_id: int, newsletter_id: int) -> list:
     # solve KeyError
     if not user_sent_newsletter(user_id, newsletter_id):
         return []
-    return user_newsletter_events[user_id][newsletter_id]
+    return user_newsletter_events[user_id]["newsletters"][newsletter_id]
 
 
 # requires that user_newsletter_map is populated and sorted
 def user_has_read_newsletter(user_id: int, newsletter_id: int) -> bool:
-    for event in get_events(user_id, newsletter_id):
-        if event["code"] in opened_codes:
-            return True
-    return False
+    if not user_sent_newsletter(user_id, newsletter_id):
+        return False
+    return newsletter_id in user_newsletter_events[user_id]["opened"]
 
 
 # requires that user has been sent newsletter
 def get_time_user_sent_newsletter(user_id: int, newsletter_id: int) -> datetime:
     # turn into datetime
     return datetime.strptime(
-        user_newsletter_events[user_id][newsletter_id][0]["timestamp"],
+        user_newsletter_events[user_id]["newsletters"][newsletter_id][0]["timestamp"],
         "%Y-%m-%d %H:%M:%S"
     )
 
@@ -154,75 +160,32 @@ def extract_event_features(
             feats[get_evt_idx(pref)] = 1
 
     # features 8: number of previous newsletters sent
-    feats[8] = len(user_newsletters[user_id]["opened"]) + len(user_newsletters[user_id]["unopened"])
+    # feats[8] = len(user_newsletters[user_id]["opened"]) + len(user_newsletters[user_id]["unopened"])
+    feats[8] = len(user_info["newsletters"])
 
     # feature 9: number of previous newsletters opened
-    feats[9] = len(user_newsletters[user_id]["opened"])
+    feats[9] = len([x for x in user_info["opened"] if x < newsletter_id])
 
     # feature 10: percentage of previous newsletters opened
     feats[10] = feats[9] / feats[8]
 
     # feature 11: time since first newsletter sent
-    first_newsletter_id = user_newsletters[user_id]["unopened"][0]\
-        if len(user_newsletters[user_id]["unopened"]) > 0\
-        else user_newsletters[user_id]["opened"][0]
+    if len(user_info["opened"]) > 0:
+        first_newsletter_id = user_info["opened"][0]
+        if len(user_info["unopened"]) > 0:
+            if user_info["unopened"][0] < first_newsletter_id:
+                first_newsletter_id = user_info["unopened"][0]
+    else:
+        first_newsletter_id = user_info["unopened"][0]
     first_newsletter_time = get_time_user_sent_newsletter(user_id, first_newsletter_id)
     current_newsletter_time = get_time_user_sent_newsletter(user_id, newsletter_id)
     # in seconds
     feats[11] = (current_newsletter_time - first_newsletter_time).total_seconds()
+    if feats[11] < 0:
+        print("first id:", first_newsletter_id, "events", user_newsletter_events[user_id]["newsletters"][first_newsletter_id])
+        print("current id:", newsletter_id, "events", user_newsletter_events[user_id]["newsletters"][newsletter_id])3
 
-    # Spacy features
+
+    # BERT features
 
     return feats
-
-
-# @profile
-# def extract_event_features(event: dict, user_event_list: list, preferences: dict) -> npt.NDArray[np.float64]:
-#     feats = np.zeros(NUM_FEATURES)
-#     # print(event)
-#     # print(preferences)
-#
-#     evt_ids = [evt["id"] for evt in user_event_list]
-#     code_indices = {}
-#     for i, evt in enumerate(user_event_list):
-#         code = evt["code"]
-#         if code not in code_indices:
-#             code_indices[code] = []
-#         code_indices[code].append(i)
-#
-#     evt_i = evt_ids.index(int(event["id"]))
-#     prev_evt_list = user_event_list[:evt_i]
-#
-#     # features 0-7: email preferences
-#     for pref in preferences:
-#         feats[get_pref_idx(pref)] = 1
-#
-#     # feature 8: number of previous user events, total
-#     # since we sorted the events by timestamp, the index of the current event is the number of previous events
-#     feats[8] = evt_i
-#
-#     # features 9-19: number of previous user events per type
-#     for evt in prev_evt_list:
-#         feats[9 + get_evt_idx(evt["code"])] += 1
-#
-#     # features 20-30: number of previous user events per type, normalized by total number of previous events
-#     for evt in prev_evt_list:
-#         feats[20 + get_evt_idx(evt["code"])] += 1 / feats[8] if feats[8] > 0 else 0
-#
-#     # features 31-41: number of previous user events per type for the past 10 events
-#     prev_10 = user_event_list[max(0, evt_i - 10):evt_i]
-#     for evt in prev_10:
-#         feats[31 + get_evt_idx(evt["code"])] += 1
-#
-#     # feature 42: time since first event
-#     # this is much faster than using datetime.strptime
-#     first_str = user_event_list[0]["timestamp"]
-#     first_evt_dt = datetime(int(first_str[:4]), int(first_str[5:7]), int(first_str[8:10]), int(first_str[11:13]),
-#                             int(first_str[14:16]), int(first_str[17:19]))
-#     curr_str = event["timestamp"]
-#     curr_evt_dt = datetime(int(curr_str[:4]), int(curr_str[5:7]), int(curr_str[8:10]), int(curr_str[11:13]),
-#                            int(curr_str[14:16]), int(curr_str[17:19]))
-#
-#     feats[42] = (curr_evt_dt - first_evt_dt).total_seconds()
-#
-#     return feats
